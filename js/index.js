f0bd936e7cb1040bae5b24349919deb0d61fc5f7
MOCAHER <moises.cano.her@gmail.com>
2022-05-02 00:05:52 -0500

    Update

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/* const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
+
+let currentDate = new Date();
+let Year = currentDate.getFullYear();
+document.getElementById(Year).textContent = Year; */
+
+/* const years = new Date()
+const last = new Date(docoment.lastModified)
+
+document.querySelector("#year").innerHTML = years.getFullYear()
+document.querySelector("#update").innerHTML = last */
+
+document.getElementById("update").textContent = fulldate;
